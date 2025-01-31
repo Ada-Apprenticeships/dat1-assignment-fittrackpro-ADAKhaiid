99b59b6046e254fb36be487c156e719cf2e52aa2
KhalidB <khalid.balesaria@ada.ac.uk>
2025-01-31 12:38:27 +0000

    payment_management complete

--- src/payment_management.sql
+++ src/payment_management.sql
@@ -9,8 +9,34 @@
 -- 1. Record a payment for a membership
 -- TODO: Write a query to record a payment for a membership
 
+INSERT INTO payments (member_id, amount, payment_date, payment_method, payment_type)
+VALUES 
+(11, 50.00, '2025-01-30 14:54:10', 'Credit Card', 'Monthly membership fee');
+-- Query was run and added to the payment table 
+
+
 -- 2. Calculate total revenue from membership fees for each month of the last year
 -- TODO: Write a query to calculate total revenue from membership fees for each month of the current year
 
+SELECT 
+    strftime('%Y-%m', payment_date) AS date,  -- Formats as YYYY-MM (e.g., 2024-01)
+    SUM(amount) AS total_revenue
+FROM 
+    payments
+GROUP BY 
+    date
+ORDER BY 
+    date;
+
 -- 3. Find all day pass purchases
 -- TODO: Write a query to find all day pass purchases
+
+SELECT 
+    payment_id,
+    amount,
+    payment_date,
+    payment_method
+FROM
+    payments
+WHERE 
+    payment_type = 'Day pass'
