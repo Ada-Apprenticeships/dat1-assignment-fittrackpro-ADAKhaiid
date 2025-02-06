4f7d9ffe03c9c18807c4cc7021ca56b9a312c736
KhalidB <khalid.balesaria@ada.ac.uk>
2025-02-06 09:46:26 +0000

    half completed class_scheduling

--- src/class_scheduling.sql
+++ src/class_scheduling.sql
@@ -3,14 +3,68 @@
 .mode column
 
 -- Enable foreign key support
+PRAGMA foreign_keys = ON;
 
 -- Class Scheduling Queries
 
 -- 1. List all classes with their instructors
 -- TODO: Write a query to list all classes with their instructors
 
+SELECT 
+    cs.class_id,
+    c.name AS class_name,
+    s.first_name || ' ' || s.last_name AS class_instructor
+FROM 
+    class_schedule AS cs
+INNER JOIN
+    staff AS s
+ON 
+    cs.staff_id = s.staff_id
+INNER JOIN
+    classes AS c
+ON 
+    cs.class_id = c.class_id;
+
 -- 2. Find available classes for a specific date
 -- TODO: Write a query to find available classes for a specific date
+
+WITH slots_available AS (
+    SELECT 
+        cs.schedule_id,
+        c.capacity - COUNT(ca.attendance_status) AS available_slots
+    FROM
+        class_attendance AS ca
+    INNER JOIN 
+        class_schedule AS cs
+    ON 
+        ca.schedule_id = cs.schedule_id
+    INNER JOIN 
+        classes AS c
+    ON  
+        cs.class_id = c.class_id
+    WHERE 
+        ca.attendance_status = 'Registered' 
+    GROUP BY cs.schedule_id, c.capacity
+)
+
+SELECT 
+    cs.class_id,
+    c.name AS class_name,
+    cs.start_time,
+    cs.end_time,
+    sa.available_slots
+FROM 
+    class_schedule AS cs
+INNER JOIN
+    classes AS c
+ON 
+    cs.class_id = c.class_id
+INNER JOIN 
+    slots_available AS sa
+ON 
+    cs.schedule_id = sa.schedule_id
+WHERE
+    cs.start_time LIKE '2025-02-01%'; 
 
 -- 3. Register a member for a class
 -- TODO: Write a query to register a member for a class
